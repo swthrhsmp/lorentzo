70c5cdab088c5ad88eff32369844315f03278422
sotiris balas <sotirismpalas@gmail.com>
2023-12-12 22:47:58 +0200

    fix container width

--- app/components/Home/Container.tsx
+++ app/components/Home/Container.tsx
@@ -2,11 +2,10 @@ import React from "react";
 
 interface Props {
   children: React.ReactNode;
-  maxWidth?: number;
 }
 
-const Container = ({ children, maxWidth = 1000 }: Props) => {
-  return <div className={`max-w-[${maxWidth}px] w-full`}>{children}</div>;
+const Container = ({ children }: Props) => {
+  return <div className={`max-w-[1000px] w-full`}>{children}</div>;
 };
 
 export default Container;
