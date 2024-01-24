5f6bc6ca0e3131a3f3c763374a1808787bc8d9c2
sotiris balas <sotirismpalas@gmail.com>
2024-01-24 23:48:35 +0200

    implement image showcase

--- app/works/ImageItem.tsx
+++ app/works/ImageItem.tsx
@@ -1,18 +1,37 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { FaRegEye } from "react-icons/fa6";
+import { FaWindowClose } from "react-icons/fa";
 
 interface Props {
   src: string;
 }
 
 const ImageItem = ({ src }: Props) => {
+  const [showImage, setShowImage] = useState(false);
   return (
-    <div className="relative aspect-square" onClick={() => alert("hi")}>
-      <div className="h-full w-full opacity-0 hover:opacity-100 absolute hover:bg-black/60 transition-all grid place-content-center cursor-pointer text-white">
-        <FaRegEye size={70} />
+    <div>
+      <div className="relative aspect-square" onClick={() => setShowImage(true)}>
+        <div className="h-full w-full opacity-0 hover:opacity-100 absolute hover:bg-black/60 transition-all grid place-content-center cursor-pointer text-white">
+          <FaRegEye size={70} />
+        </div>
+        <img src={src} className="h-full w-full object-cover" />
       </div>
-      <img src={src} className="h-full w-full" />
+
+      {showImage && (
+        <div
+          className="
+          transition-all fixed inset-0 bg-black/70 z-50 grid place-items-center"
+          onClick={() => setShowImage(false)}
+        >
+          <button className="absolute top-5 right-5 text-white">
+            <FaWindowClose size={30} />
+          </button>
+          <div className="w-[90dvw] h-[90dvh]">
+            <img src={src} className="animate-wiggle w-full h-full object-contain" />
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,17 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
+    extend: {
+      keyframes: {
+        show: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "100" },
+        },
+      },
+      animation: {
+        wiggle: "show 0.2s ease-in-out",
+      },
+    },
     fontFamily: {
       sans: "Helvetica, Arial, sans-serif",
       roboto: ["Roboto", "sans-serif"],
